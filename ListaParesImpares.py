f1b3155b80fdb71ffc0ee4c02bd4059b2a1b2f0c
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-03-14 19:09:51 -0300

    Lista pares e impares
    Lista de valores, organizados separadamente os valores PARES, IMPARES e ORDEM CRESCENTE.

---
+++ ListaParesImpares.py
@@ -0,0 +1,20 @@
+print('-='*30)
+print('{:^60}'.format('Listas com Pares e Impares'))
+print('-='*32)
+numeros = []
+pares = []
+impares = []
+for valores in range(1,8):
+    numeros.append(int(input(f'Digite {valores}º valor: ')))
+    numeros.sort()
+print('-='*30)
+for valor in range(0, len(numeros)):
+        if numeros[valor] % 2 == 0:
+               pares.append(numeros[valor])
+        else:
+              impares.append(numeros[valor])
+print(f'NÚMEROS PARES: {pares}')
+print(f'NÚMEROS ÍMPARES: {impares}')
+print('')
+print(f'Organizados em ORDEM CRESCENTE: {numeros}')
+print('-='*30)
