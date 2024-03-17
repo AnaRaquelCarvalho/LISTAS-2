463b21aed6f7caa0b7c7161e2b8f310350b25db7
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-03-16 21:07:09 -0300

    Update MATRIZ2.py

--- MATRIZ2.py
+++ MATRIZ2.py
@@ -1,4 +1,4 @@
-print('-='*20)
+rint('-='*20)
 print('{:^40}'.format('Matrizes com Listas 2'))
 print('-='*20)
 matriz = [[],[],[]]
@@ -11,4 +11,4 @@ for l in range(0,3):
         for c in range(0,3):
                 print(f'[{matriz[l][c]:^5}]', end='')
         print()
-print('-='*20)        
+print('-='*20)       
